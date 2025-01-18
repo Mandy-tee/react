0c1b63043de89e8d82e1b2fda6baca29c9ac1dda
Amanda Tagoe <nayork77@gmail.com>
2025-01-18 01:50:01 +0000

    Update 3

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -5,19 +5,31 @@ import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 import Message from './components/Message';
 import Counter from './components/Counter';
+import FunctionClick from './components/FunctionClick';
+import ClassClick from './components/ClassClick';
+import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
 
 
 function App() {
   return (
     <div className="App">
+      <NameList />
+      {/* <UserGreeting /> */}
+      {/* <ParentComponent /> */}
+      {/* <EventBind /> */}
+      {/* <FunctionClick />
+      <ClassClick /> */}
       {/* <Greet name='Bruce' heroName='Batman'>
         <p>This is children props</p>
       </Greet>
       <Greet name='Clark' heroName='Superman'>
         <button>Action</button>
       </Greet> */}
-      <Greet name='Diana' heroName='Wonder Woman' />
-      <Welcome name='Bruce' heroName='Batman' />
+      {/* <Greet name='Diana' heroName='Wonder Woman' />
+      <Welcome name='Bruce' heroName='Batman' /> */}
       {/* <Welcome name='Clark' heroName='Superman' />
       <Welcome name='Diana' heroName='Wonder Woman' /> 
       <Hello />
