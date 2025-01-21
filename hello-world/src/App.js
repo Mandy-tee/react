5de35cf260227b47889fc0b6885a75500dbc3588
Amanda Tagoe <nayork77@gmail.com>
2025-01-21 19:14:32 +0000

    Update 5

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -24,11 +24,32 @@ import ParentComp from './components/ParentComp';
 import RefsDemo from './components/RefsDemo';
 import FocusInput from './components/FocusInput';
 import FRParentInput from './components/FRParentInput';
+import PortalDemo from './components/PortalDemo';
+import Hero from './components/Hero';
+import ErrorBoundary from './components/ErrorBoundary';
+import ClickCounter from './components/ClickCounter';
+import HoverCounter from './components/HoverCounter';
 
 function App() {
   return (
     <div className="App">
-      <FRParentInput />
+      <HoverCounter />
+      <ClickCounter name='Vishwas'/>
+
+      {/* <ErrorBoundary>
+        <Hero heroName='Batman' />
+      </ErrorBoundary>
+
+      <ErrorBoundary>
+        <Hero heroName='Superman' />
+      </ErrorBoundary>
+
+      <ErrorBoundary>
+        <Hero heroName='Joker' />
+      </ErrorBoundary> */}
+
+      {/* <PortalDemo /> */}
+      {/* <FRParentInput /> */}
       {/* <FocusInput /> */}
       {/* <RefsDemo /> */}
       {/* <ParentComp /> */}
